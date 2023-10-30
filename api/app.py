7a9f23f4552945d6684fe4ffc295d92b79f10e3c
Kwabena Boakye <kwaboakye93@gmail.com>
2023-10-30 21:20:41 +0000

    Create actual flask app

--- api/app.py
+++ api/app.py
@@ -1,12 +1,39 @@
-from flask import Flask
+from flask import Flask, request, jsonify, send_file
+from apscheduler.schedulers.background import BackgroundScheduler
+from utils import compress_image, delete_old_images
+
 
 app = Flask(__name__)
 
 
 @app.route("/", methods=["GET"], strict_slashes=False)
 def index():
-    return "Home page"
+    return '<h1 style="font-family:monospace">Welcome to ImgKmprssr&#174;</h1>'
+
+
+@app.route("/compress-image", methods=["POST"], strict_slashes=False)
+def handle_compress():
+    if "image" not in request.files:
+        return jsonify({"error": "No Image provided"}), 400
+
+    image = request.files.get("image")
+
+    if image.filename == "":
+        return jsonify({"error": "No selected image file"}), 400
+    try:
+        new_filename = f"images/{image.filename.split('.')[0]}_cmprssd.jpeg"
+
+        compressed_path = compress_image(image, new_filename)
+    except Exception as e:
+        return jsonify({"error": f"{str(e)}"}), 400
+
+    return send_file(compressed_path, as_attachment=True)
+
+
+scheduler = BackgroundScheduler(daemon=True)
+scheduler.add_job(delete_old_images, "interval", days=1)  # Run daily
 
 
 if __name__ == "__main__":
+    scheduler.start()
     app.run(host="0.0.0.0", port=5000, threaded=True, debug=True)
