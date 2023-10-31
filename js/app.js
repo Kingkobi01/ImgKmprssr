a06491e36f56b15439994c52156d0dfc94e056ad
Dcardinal042 <samokoh40@gmail.com>
2023-10-31 01:45:15 -0700

    ImgKmprssr

---
+++ js/app.js
@@ -0,0 +1,31 @@
+// This JavaScript code enhances the functionality of the landing page.
+
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.querySelector('form');
+    const compressedImage = document.getElementById('compressed-image');
+    const downloadLink = document.getElementById('download-link');
+
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const imageInput = document.getElementById('image');
+        const outputImage = document.getElementById('output-image');
+        outputImage.style.display = 'none';
+
+        const formData = new FormData(form);
+
+        fetch('/compress', {
+            method: 'POST',
+            body: formData,
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (data.success) {
+                compressedImage.src = data.compressed_url;
+                outputImage.style.display = 'block';
+                downloadLink.href = data.compressed_url;
+            }
+        })
+        .catch(error => console.error(error));
+    });
+});
+
