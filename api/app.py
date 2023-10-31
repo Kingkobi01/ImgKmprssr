faf5b5a77d59ffd93cac415cf88ee76f9b39b0c3
Kwabena Boakye <kwaboakye93@gmail.com>
2023-10-31 15:02:59 +0000

    debugging

--- api/app.py
+++ api/app.py
@@ -16,6 +16,7 @@ def index():
 
 @app.route("/compress", methods=["POST"], strict_slashes=False)
 def handle_compress():
+    print(request)
     if "image" not in request.files:
         return jsonify({"error": "No Image provided"}), 400
 
